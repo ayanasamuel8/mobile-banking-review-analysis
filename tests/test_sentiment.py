b71163576f81451862a7db93e57dd46c36656974
Ayana <ayanasamuel8@gmail.com>
2025-06-09 23:38:55 +0300

    task-2 concluded

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,11 +1,11 @@
 import pytest
 from src.task_2.sentiment import analyze_sentiment
 
-
 @pytest.mark.parametrize("text,expected", [
     ("This app is amazing!", "positive"),
     ("It’s okay, nothing special.", "neutral"),
     ("Worst banking experience ever.", "negative")
 ])
 def test_sentiment_classification(text, expected):
-    assert analyze_sentiment(text) == expected
+    label, _ = analyze_sentiment(text)
+    assert label == expected
